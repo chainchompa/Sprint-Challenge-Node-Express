6ec35320bfd337252b9c45d7833ae86fe0efb166
chainchompa <chainchompa@gmail.com>
2018-07-27 11:54:57 -0400

    able to add new projects and actions

--- routes/actionRoutes.js
+++ routes/actionRoutes.js
@@ -15,4 +15,21 @@ router.get('/', (req, res) => {
   })
 })
 
+//Add a new action
+router.post('/', (req, res) => {
+  const { project_id, description, notes } = req.body;
+  if (!project_id || !description || !notes) {
+    res.status(400).json({ errorMessage: "Please provide a project ID, description, and some notes for the action." })
+    return;
+  }
+  actionModel
+  .insert({ project_id, description, notes })
+  .then(action => {
+    res.status(201).json({ action })
+  })
+  .catch(error => {
+    res.status(500).json({ error: "There was an error while saving the action to the database" })
+  })
+})
+
 module.exports = router;
--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -15,6 +15,7 @@ router.get('/', (req, res) => {
   })
 })
 
+//View a project's actions
 router.get('/:id', (req, res) => {
   const { id } = req.params;
   projectModel
@@ -28,6 +29,23 @@ router.get('/:id', (req, res) => {
   })
   .catch(error => {
     res.status(500).json({ error: "The project information could not be retrieved." })
+  })
+})
+
+//Add a new project
+router.post('/', (req, res) => {
+  const { name, description } = req.body;
+  if (!name || !description) {
+    res.status(400).json({ errorMessage: "Please provide a name and description for the project." })
+    return;
+  }
+  projectModel
+  .insert({ name, description })
+  .then(project => {
+    res.status(201).json({ project })
+  })
+  .catch(error => {
+    res.status(500).json({ error: "There was an error while saving the project to the database" })
   })
 })
 
