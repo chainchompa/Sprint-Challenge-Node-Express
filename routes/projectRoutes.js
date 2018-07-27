e0efdb0f3c4ad67af19806d591a3e43ebda6d8bb
chainchompa <chainchompa@gmail.com>
2018-07-27 11:27:24 -0400

    set up routers for actions and projects

---
+++ routes/projectRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const projectModel = require('../data/helpers/projectModel.js');
+
+//View actions
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,10 +3,15 @@ const server = express();
 const morgan = require('morgan')
 const helmet = require('helmet');
 
+const projectRoutes = require('./routes/projectRoutes');
+const actionRoutes = require('./routes/actionRoutes');
+
 server.use(express.json());
 server.use(morgan('dev'));
 server.use(helmet());
 
+server.use('/projects', projectRoutes);
+server.use('/actions', actionRoutes);
 server.use('/', (req, res) => res.send('API up and running!'));
 
 server.listen(8000, () => console.log('API running on port 8000'));
